746471a1d6a1636c2dbbe9122f25948dda9c5f0a
JuliaGalich <julia.galich2011@gmail.com>
2025-01-11 14:54:47 +0200

    added design(part 2)

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    Name = models.CharField(max_length=50)
+    Surname = models.CharField(max_length=50)
+    Phone = models.CharField(max_length=15)
+    Email = models.EmailField()
+    Photo = models.ImageField(upload_to='contacts_photo/', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.Name} {self.Surname}'
