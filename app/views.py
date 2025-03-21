a2ade0781e232a6df9111b85e78889fff7803d83
JuliaGalich <julia.galich2011@gmail.com>
2025-03-21 14:24:40 +0200

    fixing

--- app/views.py
+++ app/views.py
@@ -5,14 +5,14 @@ from .forms import Contacts_form
 # Create your views here.
 def contact_list(request):
     contacts = Contact.objects.all()
-    return render(request, 'index.html', {'contacts': contacts})
+    return render(request, 'contact_list.html', {'contacts': contacts})
 
 def addnew(request):
     if request.method == 'POST':
         form = Contacts_form(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('contact_list')
+        return redirect('contact_list')
     else:
         form = Contacts_form()
         return render(request, 'contact_form.html', {'form': form})
--- project/urls.py
+++ project/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.contact_list, name='home'),
     path("contact_form/", views.addnew, name="contact_form"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, documenr_root = settings.MEDIA_ROOT)
